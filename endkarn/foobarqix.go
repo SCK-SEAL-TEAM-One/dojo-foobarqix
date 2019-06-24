0a8e0f8385b5f1f4fb57b7c7b5d309f6701b3da8
endkarn <end_karn@hotmail.com>
2019-06-24 09:51:10 +0700

    ADD : เพิ่ม Test Case

--- endkarn/foobarqix.go
+++ endkarn/foobarqix.go
@@ -1,5 +1,26 @@
 package endkarn
 
+import "strconv"
+
 func ComputeFooBarQix(input string) interface{} {
-	return "FooFoo"
+	int, _ := strconv.Atoi(input)
+	if input == "3" {
+		return "FooFoo"
+	}
+	if int%3 == 0 {
+		return "Foo"
+	}
+	if input == "5" {
+		return "BarBar"
+	}
+	if int%5 == 0 {
+		return "Bar"
+	}
+	if input == "7" {
+		return "QixQix"
+	}
+	if int%7 == 0 {
+		return "Qix"
+	}
+	return input
 }
--- endkarn/foobarqix_test.go
+++ endkarn/foobarqix_test.go
@@ -11,3 +11,63 @@ func Test_FooBarQix_Put_3_Should_Return_FooFoo(t *testing.T) {
 		t.Errorf(" Expected value %v but got %v ", expectedResult, actualResult)
 	}
 }
+
+func Test_FooBarQix_Put_5_Should_Return_BarBar(t *testing.T) {
+	expectedResult := "BarBar"
+
+	actualResult := ComputeFooBarQix("5")
+
+	if expectedResult != actualResult {
+		t.Errorf(" Expected value %v but got %v ", expectedResult, actualResult)
+	}
+}
+
+func Test_FooBarQix_Put_7_Should_Return_QixQix(t *testing.T) {
+	expectedResult := "QixQix"
+
+	actualResult := ComputeFooBarQix("7")
+
+	if expectedResult != actualResult {
+		t.Errorf(" Expected value %v but got %v ", expectedResult, actualResult)
+	}
+}
+
+func Test_FooBarQix_Put_1_Should_Return_1(t *testing.T) {
+	expectedResult := "1"
+
+	actualResult := ComputeFooBarQix("1")
+
+	if expectedResult != actualResult {
+		t.Errorf(" Expected value %v but got %v ", expectedResult, actualResult)
+	}
+}
+
+func Test_FooBarQix_Put_6_Should_Return_Foo(t *testing.T) {
+	expectedResult := "Foo"
+
+	actualResult := ComputeFooBarQix("6")
+
+	if expectedResult != actualResult {
+		t.Errorf(" Expected value %v but got %v ", expectedResult, actualResult)
+	}
+}
+
+func Test_FooBarQix_Put_10_Should_Return_Bar(t *testing.T) {
+	expectedResult := "Bar"
+
+	actualResult := ComputeFooBarQix("10")
+
+	if expectedResult != actualResult {
+		t.Errorf(" Expected value %v but got %v ", expectedResult, actualResult)
+	}
+}
+
+func Test_FooBarQix_Put_14_Should_Return_Qix(t *testing.T) {
+	expectedResult := "Qix"
+
+	actualResult := ComputeFooBarQix("14")
+
+	if expectedResult != actualResult {
+		t.Errorf(" Expected value %v but got %v ", expectedResult, actualResult)
+	}
+}
